9df011ff5e5143abef0ebc2f571df4657d29964e
Daniel <dtaylor6@umbc.edu>
2023-04-06 00:15:40 -0400

    Spotify login route now redirects back to frontend with token

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 const express = require('express')
 require('express-async-errors')
 const app = express()
+//const inProd = process.env.NODE_ENV === 'production'
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
 const spotifyRouter = require('./controllers/spotify')
 const discogsRouter = require('./controllers/discogs')
 const middleware = require('./utils/middleware')
 
-app.use(cors())
+app.use(cors()) // TODO: fix cors policy so it's not a wildcard
+app.use(cookieParser())
 app.use(express.json())
 
 app.use(middleware.requestLogger)
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
 require ('dotenv').config()
 
 const PORT = process.env.PORT
+const FRONTEND_PORT = process.env.FRONTEND_PORT
 
 const POSTGRES_SECRET = process.env.POSTGRES_SECRET
 
@@ -12,6 +13,7 @@ const DISCOGS_SECRET = process.env.DISCOGS_SECRET
 
 module.exports = {
   PORT,
+  FRONTEND_PORT,
   POSTGRES_SECRET,
   SPOTIFY_CLIENT_ID,
   SPOTIFY_SECRET,
