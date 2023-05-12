c42dae1e788612516be1c37d41b88589a80dfa79
Daniel <dtaylor6@umbc.edu>
2023-05-11 20:32:43 -0400

    Call next in error handler middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -20,20 +20,21 @@ const errorHandler = (err, req, res, next) => {
     logger.error(err.response.data)
     logger.error(err.response.status)
     logger.error(err.response.headers)
-    return res.status(err.response.status).send(err.response.data)
+    res.status(err.response.status).send(err.response.data)
   }
   else if (err.request) {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser
     // and an instance of http.ClientRequest in node.js
     logger.error(err.request)
-    return res.status(500).send(err.request)
+    res.status(500).send(err.request)
   }
   else {
     // Something happened in setting up the request that triggered an Error
     logger.error('Error', err.message)
-    return res.status(500).send('Something went wrong with the dedeluxify-backend')
+    res.status(500).send('Something went wrong with the dedeluxify-backend')
   }
+  next()
 }
 
 module.exports = {
