ca0e158a24f9888b156503391b59709fc5e416c8
Daniel <dtaylor6@umbc.edu>
2023-07-09 18:53:31 -0400

    Remove comments and specify node version

--- controllers/trackPreferences.js
+++ controllers/trackPreferences.js
@@ -13,8 +13,6 @@ import {
 trackPreferencesRouter.use(getSpotifyUser);
 
 const albumIdExtractor = (req, res, next) => {
-  console.log('uri: ', req.query.uri);
-  console.log('typeof uri', typeof req.query.uri);
   const uri = req.body.uri || req.query.uri;
   if (!uri) {
     return res.status(400).send('No Spotify URI given');
