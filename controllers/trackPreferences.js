9d009e99d0c9d83d76ed43c90f2949781a9a9682
Daniel <dtaylor6@umbc.edu>
2023-07-09 01:04:18 -0400

    add temp log statement for debug

--- controllers/trackPreferences.js
+++ controllers/trackPreferences.js
@@ -13,6 +13,8 @@ import {
 trackPreferencesRouter.use(getSpotifyUser);
 
 const albumIdExtractor = (req, res, next) => {
+  console.log('uri: ', uri);
+  console.log('typeof uri', typeof uri);
   const uri = req.body.uri || req.query.uri;
   if (!uri) {
     return res.status(400).send('No Spotify URI given');
