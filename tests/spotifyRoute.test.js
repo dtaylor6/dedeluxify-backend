c00364e1396183277dee38f1d6ff3d83a3bfac54
Daniel <dtaylor6@umbc.edu>
2023-07-31 13:55:37 -0400

    Add more search album tests

--- tests/spotifyRoute.test.js
+++ tests/spotifyRoute.test.js
@@ -17,6 +17,36 @@ describe('search tests', () => {
       .get('/api/spotify/search')
       .expect(401);
   });
+
+  test('search route fails with bad auth header', async () => {
+    await api
+      .get('/api/spotify/search')
+      .set({ authorization: clientToken.slice(7,) })
+      .expect(401);
+  });
+
+  test('search route fails with empty query', async () => {
+    await api
+      .get('/api/spotify/search')
+      .set({ authorization: clientToken })
+      .expect(400);
+  });
+
+  test('search for album', async () => {
+    await api
+      .get('/api/spotify/search?q=nevermind')
+      .set({ authorization: clientToken })
+      .expect(200);
+  });
+
+  test('search for album', async () => {
+    const response = await api
+      .get('/api/spotify/search?q=thriller')
+      .set({ authorization: clientToken })
+      .expect(200);
+
+    //console.log(response.body);
+  });
 });
 
 afterAll(async () => {
